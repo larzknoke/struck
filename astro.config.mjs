d86c1087ed7707b7eafb132366bd2bc7045c1d14
Lars Knoke <larz.knoke@gmail.com>
2024-12-26 12:50:07 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   redirects: {
     "/jobs": "/jobs/geselle",
+    "/leistungen": "/leistungen/dacheindeckung",
   },
 });
