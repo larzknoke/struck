6f3491e436c9b3e504acb2e2053b3ab1cb1c1391
Lars Knoke <larz.knoke@gmail.com>
2025-01-15 21:26:00 +0100

    mobileNav & compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/jobs": "/jobs/geselle",
     "/leistungen": "/leistungen/dacheindeckung",
   },
+
+  integrations: [playformCompress()],
 });
